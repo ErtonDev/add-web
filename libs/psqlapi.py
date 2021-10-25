ae60e472e5f1a9eec9c3263ec2f6c8d4581e3e93
MarcBen05 <marcben05@gmail.com>
2021-10-25 18:14:38 +0200

    Added essential functions in API
    Added the "connect", "insert" and "get" functions. Trying to implement "put" function at the moment.

--- libs/psqlapi.py
+++ libs/psqlapi.py
@@ -1,2 +1,47 @@
-def hellothere():
-    print("Hello There!")
+import psycopg2
+
+#CONNECT Function
+def connect():
+    print("Connecting to database . . . ")
+    try:
+        conn = psycopg2.connect(host="ec2-54-73-68-39.eu-west-1.compute.amazonaws.com", user="zinavvopoesljt",
+                    password="e9acadc681b670392c65be283556f2b67f34875f6fbbc56d4055114f4f39dff9", dbname="d2qnnn465f5n23")
+        print("CONNECTED!")
+        return conn
+    except Exception as error:
+        print(f"ERROR: Failed to connect to database!\nERROR INFO: {error}\nEXCEPTION TYPE: {type(error)}")
+
+#POST Function
+def post(conn, table, param, values):
+    try:
+        cur = conn.cursor();
+        cur.execute(f"""INSERT INTO {table}({param})
+        VALUES({values})""")
+        conn.commit()
+        cur.close()
+    except Exception as error:
+        print(f"ERROR: Failed to insert data!\nERROR INFO: {error}\nEXCEPTION TYPE: {type(error)}\n-------------------")
+
+#GET Function
+def get(conn, table, param):
+    try:
+        cur = conn.cursor();
+        cur.execute(f"""SELECT {param} FROM {table} ORDER BY user_id""")
+        result = cur.fetchall()
+        conn.commit()
+        cur.close()
+        return result
+    except Exception as error:
+        print(f"ERROR: Failed to get data!\nERROR INFO: {error}\nEXCEPTION TYPE: {type(error)}\n-------------------")
+
+#PUT Function
+def put(conn, table, param):
+    try:
+        cur = conn.cursor();
+        cur.execute(f"""UPDATE {param} FROM {table} ORDER BY user_id""")
+        conn.commit()
+        cur.close()
+    except Exception as error:
+        print(f"ERROR: Failed to put data!\nERROR INFO: {error}\nEXCEPTION TYPE: {type(error)}\n-------------------")
+conn = connect()
+
