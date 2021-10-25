53e2c5d1839555fb46935244e5d3ad2d503d3403
MarcBen05 <marcben05@gmail.com>
2021-10-25 18:38:38 +0200

    Improved API
    Added the "put" function and upgraded the "get" function to use IDs and return strings.

--- libs/psqlapi.py
+++ libs/psqlapi.py
@@ -2,14 +2,14 @@ import psycopg2
 
 #CONNECT Function
 def connect():
-    print("Connecting to database . . . ")
+    print(". . . Connecting to database . . . ")
     try:
         conn = psycopg2.connect(host="ec2-54-73-68-39.eu-west-1.compute.amazonaws.com", user="zinavvopoesljt",
                     password="e9acadc681b670392c65be283556f2b67f34875f6fbbc56d4055114f4f39dff9", dbname="d2qnnn465f5n23")
         print("CONNECTED!")
         return conn
     except Exception as error:
-        print(f"ERROR: Failed to connect to database!\nERROR INFO: {error}\nEXCEPTION TYPE: {type(error)}")
+        print(f"ERROR: Failed to connect to database!\nERROR INFO: {error}\nEXCEPTION TYPE: {type(error)}\n-------------------")
 
 #POST Function
 def post(conn, table, param, values):
@@ -23,25 +23,30 @@ def post(conn, table, param, values):
         print(f"ERROR: Failed to insert data!\nERROR INFO: {error}\nEXCEPTION TYPE: {type(error)}\n-------------------")
 
 #GET Function
-def get(conn, table, param):
+def get(conn, id, table, param):
     try:
         cur = conn.cursor();
-        cur.execute(f"""SELECT {param} FROM {table} ORDER BY user_id""")
+        cur.execute(f"""SELECT {param} FROM {table} WHERE user_id = {id}""")
         result = cur.fetchall()
         conn.commit()
         cur.close()
-        return result
+        #Result is a list of tuples, so to get the string we have to access both with index 0
+        return result[0][0]
     except Exception as error:
         print(f"ERROR: Failed to get data!\nERROR INFO: {error}\nEXCEPTION TYPE: {type(error)}\n-------------------")
 
 #PUT Function
-def put(conn, table, param):
+def put(conn, id, table, param, values):
     try:
         cur = conn.cursor();
-        cur.execute(f"""UPDATE {param} FROM {table} ORDER BY user_id""")
+        cur.execute(f"""UPDATE {table} SET {param} = {values} WHERE user_id = {id}""")
         conn.commit()
         cur.close()
     except Exception as error:
         print(f"ERROR: Failed to put data!\nERROR INFO: {error}\nEXCEPTION TYPE: {type(error)}\n-------------------")
+
+
 conn = connect()
 
+result = get(conn, "124", "tab", "user_name")
+print(result)
