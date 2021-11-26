7a52ef2446ca105272b5c79ace36c21ae9bc2772
Erton <ericsanchezjimenez35@gmail.com>
2021-11-27 00:50:54 +0100

    Started coding a way of navigating the CC
    I started implementating a way of navigating the control center. Basically it consists of a DB row selection. It should also be useful for .txt files that aren't uploaded in the DB. I've also created a log file to do some testing, it is temporal. **If you want to read the script you will find some of my notes as well as the things that need to be implementated. Feel free to add any of these features.**

--- cc.py
+++ cc.py
@@ -1,4 +1,6 @@
 import web.psqlapi
+import msvcrt
+import io
 
 from datetime import datetime
 
@@ -21,6 +23,95 @@ console = Console()
 install(show_locals=True)
 
 
+# display the gui header
+class Header:
+
+	def __rich__(self) -> Panel:
+		grid = Table.grid(expand=True)
+		grid.add_column(justify="center", ratio=1)
+		grid.add_column(justify="right")
+		grid.add_row(
+			"[b]CC[/b] Control Center",
+			datetime.now().ctime().replace(":", "[blink]:[/]")
+		)
+		return Panel(grid, style="white on blue")
+
+# selector methods as well as position handling
+class Selector:
+	# FIXME(Erton): Currently giving a strange error, stack overflow says indentations may be the issue
+	# sublime text has been giving me some problems with indentations so I may want to try using another code editor
+
+	def __init__(self):
+		self.position = 0
+		self.selections = ['100.txt', '101.txt', '102.txt'] # TODO(Erton): Provisional files for testing
+		self.path = "../profile/"
+
+	def __rich__(self) -> Panel:
+		# NOTE(Erton): Provisional code for testing
+		self.selector = Table.grid()
+		self.selector.add_column(style="yellow", no_wrap=True)
+		self.selector.add_column(justify="right", style="cyan")
+		self.set_pos()
+		self.selector = Align.center(self.selector)
+
+		return Panel(self.selector,
+			title="Selector",
+			box=box.ROUNDED,
+			padding=(2, 2),
+			border_style="bright_blue")
+
+	# TODO(Erton): Get path from the directory watching
+	def get_path(self):
+		pass
+
+	# TODO(Erton): Has to change selections inside class, selections are checked outside
+	def get_sel(self, selections):
+		# something like self.selections = selections
+		pass
+
+	# TODO(Erton): Update the selector to show the highlighting
+	def set_pos(self):
+
+		# adds rows inside selector
+		self.pos_counter = 0 
+		for i in self.selections:
+			# underlines i in position
+			if self.pos_counter == self.position:
+				# underlined
+				self.selector.add_row(f"[u]{i}[/u]", " -> " + self.path)
+			else:
+				self.selector.add_row(i, " -> " + self.path)
+			self.pos_counter += 1
+		self.pos_counter = 0
+
+	# TODO(Erton): Change highlighting when going up in the selector
+	def up_pos(self):
+		global selector
+
+		if self.position == 0:
+			self.position = len(self.selections)
+			self.set_pos()
+		else:
+			self.position -= 1
+			self.set_pos()
+
+	# TODO(Erton): Change highlighting when going down in the selector
+	def down_pos(self):
+		global selector
+
+		if self.position == len(self.selections):
+			self.position = 0
+			self.set_pos()
+		else:
+			self.position += 1
+			self.set_pos()
+
+	# TODO(Erton): Makes a selection, so it must change the entire display
+	def select(self):
+		pass
+
+
+# defines the main layout of the gui
 def make_layout() -> Layout:
 	# Define the layout
 	layout = Layout(name="Control Center")
@@ -43,11 +134,13 @@ def make_layout() -> Layout:
 
 	return layout
 
+# get the predefined values in ccvar.txt
 def get_variables():
 	# TODO(Erton): Code the API connection to GET values from DB
 	pass
 
-def show_variable_display() -> Panel:
+# display body content
+def show_body() -> Panel:
     # variable display
     table = Table(title="[b]----- VARIABLE DISPLAY -----[/]")
 
@@ -72,24 +165,38 @@ def show_variable_display() -> Panel:
     	border_style="bright_blue")
 
 
-class Header:
-
-	def __rich__(self) -> Panel:
-		grid = Table.grid(expand=True)
-		grid.add_column(justify="center", ratio=1)
-		grid.add_column(justify="right")
-		grid.add_row(
-			"[b]CC[/b] Control Center",
-			datetime.now().ctime().replace(":", "[blink]:[/]")
-		)
-		return Panel(grid, style="white on blue")
-
-
 layout = make_layout()
 layout["header"].update(Header())
-layout["body"].update(show_variable_display())
-print(layout)
+layout["body"].update(show_body())
+layout["box1"].update(Selector())
+print(layout) 
 
+sel = Selector()
+
+# main loop with live functionality
 with Live(layout, refresh_per_second=10, screen=True):
 	while True:
+
+		# key input check
+		if msvcrt.kbhit():
+			k = msvcrt.getch()
+
+			log = io.open("ccdata/runlog.txt", 'a')
+			log.write("\n" + str(k))
+			log.close()
+
+			if str(k) == "b'w'":
+				sel.up_pos()
+
+				log = io.open("ccdata/runlog.txt", 'a')
+				log.write("\ncall up")
+				log.close()
+
+			if str(k) == "b's'":
+				sel.down_pos()
+
+				log = io.open("ccdata/runlog.txt", 'a')
+				log.write("\ncall down")
+				log.close()
+
 		sleep(0.1)
