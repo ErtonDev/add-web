68085f8c4250f81c36e7bbdb72961412224bda64
Erton <ericsanchezjimenez35@gmail.com>
2022-03-07 12:46:21 +0100

    Added paths to flask code
    I added the paths for the different templates of our website.

--- web/app.py
+++ web/app.py
@@ -6,10 +6,29 @@ from psqlapi import connect, post_bot, post_user, get_bot, get_user, put_bot, pu
 
 app = Flask(__name__)
 
+
+
 # home
 @app.route("/")
 def index():
-    return render_template("index.html", name="home")
+    return render_template("index.html", name="Home")
+
+# bot
+@app.route("/bot")
+def bot():
+    return reder_template("bot.html", name="El Hijo")
+
+# profile
+@app.route("/profile")
+def profile():
+    return render_template("profile.html", name="Profile")
+
+# library
+@app.route("/library")
+def profile():
+    return render_template("library.html", name="ADDeditorial")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
